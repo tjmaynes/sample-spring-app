741b0d3383bdb46b81a085e0cb7ec77527e121a3
TJ Maynes (Personal) <tj@tjmaynes.com>
2020-01-06 17:59:54 -0500

    KISS

--- recipe-book-core/src/main/kotlin/com/tjmaynes/recipebook/core/service/IngredientService.kt
+++ recipe-book-core/src/main/kotlin/com/tjmaynes/recipebook/core/service/IngredientService.kt
@@ -4,6 +4,7 @@ import arrow.core.Left
 import com.tjmaynes.recipebook.core.domain.Ingredient
 import com.tjmaynes.recipebook.core.domain.validate
 import com.tjmaynes.recipebook.core.types.*
+import com.tjmaynes.recipebook.core.validation.ValidationErrors
 
 class IngredientService(private val repository: IRepository<Ingredient>) : IService<Ingredient> {
     override suspend fun getAll(request: PaginatedRequest): ServiceResult<PaginatedResponse<Ingredient>> =
@@ -26,4 +27,20 @@ class IngredientService(private val repository: IRepository<Ingredient>) : IServ
 
     override suspend fun removeItem(id: String): ServiceResult<String> =
         repository.removeItem(id).mapLeft(::handleRepositoryException)
+
+    private fun handleBadRequestException(errors: ValidationErrors) = ServiceException(
+        status = ServiceException.StatusCode.BadRequest,
+        messages = errors
+    )
+
+    private fun handleRepositoryException(exception: RepositoryException) = when (exception.status) {
+        RepositoryException.StatusCode.NotFound -> ServiceException(
+            status = ServiceException.StatusCode.NotFound,
+            messages = exception.messages
+        )
+        RepositoryException.StatusCode.Unknown -> ServiceException(
+            status = ServiceException.StatusCode.Unknown,
+            messages = exception.messages
+        )
+    }
 }
