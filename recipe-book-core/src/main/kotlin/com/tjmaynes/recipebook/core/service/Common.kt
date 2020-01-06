f473994f4a9d6451660fc1f4002086fd0bb5207f
TJ Maynes (Personal) <tj@tjmaynes.com>
2020-01-06 17:12:41 -0500

    Refactor for readability

--- recipe-book-core/src/main/kotlin/com/tjmaynes/recipebook/core/service/Common.kt
+++ recipe-book-core/src/main/kotlin/com/tjmaynes/recipebook/core/service/Common.kt
@@ -5,17 +5,17 @@ import com.tjmaynes.recipebook.core.types.ServiceException
 import com.tjmaynes.recipebook.core.validation.ValidationErrors
 
 fun handleBadRequestException(errors: ValidationErrors) = ServiceException(
-        status = ServiceException.StatusCode.BadRequest,
-        messages = errors
+    status = ServiceException.StatusCode.BadRequest,
+    messages = errors
 )
 
 fun handleRepositoryException(exception: RepositoryException) = when (exception.status) {
     RepositoryException.StatusCode.NotFound -> ServiceException(
-            status = ServiceException.StatusCode.NotFound,
-            messages = exception.messages
+        status = ServiceException.StatusCode.NotFound,
+        messages = exception.messages
     )
     RepositoryException.StatusCode.Unknown -> ServiceException(
-            status = ServiceException.StatusCode.Unknown,
-            messages = exception.messages
+        status = ServiceException.StatusCode.Unknown,
+        messages = exception.messages
     )
 }
--- recipe-book-core/src/main/kotlin/com/tjmaynes/recipebook/core/service/IngredientService.kt
+++ recipe-book-core/src/main/kotlin/com/tjmaynes/recipebook/core/service/IngredientService.kt
@@ -1,35 +1,29 @@
 package com.tjmaynes.recipebook.core.service
 
 import arrow.core.Left
-import arrow.fx.IO
-import arrow.fx.extensions.fx
 import com.tjmaynes.recipebook.core.domain.Ingredient
 import com.tjmaynes.recipebook.core.domain.validate
 import com.tjmaynes.recipebook.core.types.*
 
 class IngredientService(private val repository: IRepository<Ingredient>) : IService<Ingredient> {
     override suspend fun getAll(request: PaginatedRequest): ServiceResult<PaginatedResponse<Ingredient>> =
-            repository.getAll(request).mapLeft { handleRepositoryException(it) }
+        repository.getAll(request).mapLeft(::handleRepositoryException)
 
     override suspend fun getById(id: String): ServiceResult<Ingredient> =
-            repository.getById(id).mapLeft { handleRepositoryException(it) }
+        repository.getById(id).mapLeft(::handleRepositoryException)
 
     override suspend fun addItem(item: Ingredient): ServiceResult<Ingredient> =
-            IO.fx {
-                item.validate().fold(
-                        { Left(handleBadRequestException(it)) },
-                        { !effect { repository.addItem(it).mapLeft { handleRepositoryException(it) } } }
-                )
-            }.suspended()
+        item.validate().fold(
+            { Left(handleBadRequestException(it)) },
+            { repository.addItem(it).mapLeft(::handleRepositoryException) }
+        )
 
     override suspend fun updateItem(item: Ingredient): ServiceResult<Ingredient> =
-            IO.fx {
-                item.validate().fold(
-                        { Left(handleBadRequestException(it)) },
-                        { !effect { repository.updateItem(it).mapLeft { handleRepositoryException(it) } } }
-                )
-            }.suspended()
+        item.validate().fold(
+            { Left(handleBadRequestException(it)) },
+            { repository.updateItem(it).mapLeft(::handleRepositoryException) }
+        )
 
     override suspend fun removeItem(id: String): ServiceResult<String> =
-            repository.removeItem(id).mapLeft { handleRepositoryException(it) }
+        repository.removeItem(id).mapLeft(::handleRepositoryException)
 }
